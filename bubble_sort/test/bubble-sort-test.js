fbd1e5947ea8adb0bf8109e11c7e29096df76047
squeemishly <squeemishly@gmail.com>
2017-08-12 18:55:25 -0600

    expanded test to do a number sort

--- bubble_sort/test/bubble-sort-test.js
+++ bubble_sort/test/bubble-sort-test.js
@@ -4,5 +4,6 @@ var bubbleSort = require('../bubble-sort')
 describe('it has functions', function() {
   it('can sort a list', function() {
     assert.deepEqual(bubbleSort(["d", "b", "a", "c"]), ["a", "b", "c", "d"])
+    assert.deepEqual(bubbleSort([5, 8, 2, 4, 9]), [2, 4, 5, 8, 9])
   })
 })
